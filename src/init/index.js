a4d13f904e6167ebb09a52fd0d8e5f846f9ec475
Samir M <67711948+samirm00@users.noreply.github.com>
2021-01-31 16:34:58 +0100

    Initial commit

---
+++ src/init/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import '../listeners/listEnter.js'
+
+// import all listener files so they can attach to the DOM
+
+// does your project have code that executes when the document or window are ready?
+// that code can go here
