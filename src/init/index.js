242b1743fa712ffea4e52f34a14f2212193c26c5
samirm00 <sammoham.be@gmail.com>
2021-02-05 16:15:45 +0100

    add all  listeners

--- src/init/index.js
+++ src/init/index.js
@@ -6,3 +6,9 @@
 
 // does your project have code that executes when the document or window are ready?
 // that code can go here
+
+import '../listeners/listenerOne.js';
+import '../listeners/listenerTwo.js';
+import '../listeners/listenerThree.js';
+import '../listeners/listenerFour.js'
+import '../listeners/listenerFive.js'
